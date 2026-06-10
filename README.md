8a90d1b1ad385cafada816ca5df06fd7cc4ea005
Beatriz Alves <beatrizalves.san00@gmail.com>
2026-06-10 00:15:22 -0300

    Criação das opções

---
+++ README.md
@@ -0,0 +1,12 @@
+Olá esse projeto é um Catálogo de filme.
+Aqui voce pode adicionar os filmes, dar uma nota, listar eles e por fim ver quer foi o melhor filme cadastrado!
+é um projeto simples que eu estou fazendo para treinar mais a prática da programação e do versionamento(git). 
+
+Estou práticando: Python 
+- Variáveis
+- Input
+- If
+- Loops
+- Listas
+- Dicionários
+- Funções
---
+++ catalogo.py
@@ -0,0 +1,24 @@
+filmes = []
+
+while True:
+    print("\n=== CATÁLOGO DE FILMES ===")
+    print("1 - Adicionar filme")
+    print("2 - Ver filmes")
+    print("3 - Sair")
+
+    opcao = input("Escolha: ")
+
+    if opcao == "1":
+        nome = input("Nome do filme: ")
+
+        filme = {
+            "nome": nome
+        }
+
+        filmes.append(filme)
+
+    elif opcao == "2":
+        print(filmes)
+
+    elif opcao == "3":
+        break
